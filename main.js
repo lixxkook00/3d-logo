1b63d4ecf0fdb7080703483a03ac590767101988
Linh <lixxkook@Macbut-M1-cua-Linh.local>
2022-12-06 23:54:27 +0700

    ip[daye

--- main.js
+++ main.js
@@ -3,16 +3,15 @@ const modelViewerTexture = document.querySelector("#logo");
 modelViewerTexture.addEventListener("load", () => {
     const material = modelViewerTexture.model.materials[0];
     const material2 = modelViewerTexture.model.materials[1];
-    
-    // console.log(material);
-    console.log("material",material);
+
     // material.pbrMetallicRoughness.setBaseColorFactor("../assest/do.png");
 
     const createTexture = async () => {
         const texture = await modelViewerTexture.createTexture("./assets/do.png");    
-        const texture2 = await modelViewerTexture.createTexture("./assets/xanh.png");    
+        const texture2 = await modelViewerTexture.createTexture("./assets/xanh.png");  
         
-        console.log("texture",texture);
+        console.log(modelViewerTexture.availableVariants)
+        
         material.pbrMetallicRoughness["baseColorTexture"].setTexture(texture);
         material2.pbrMetallicRoughness["baseColorTexture"].setTexture(texture2);
     }
