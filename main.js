93148cbd187340cfe3db35283044fa03999af0d7
Linh <lixxkook@Macbut-M1-cua-Linh.local>
2022-12-06 12:23:18 +0700

    update

--- main.js
+++ main.js
@@ -18,17 +18,4 @@ modelViewerTexture.addEventListener("load", () => {
     }
 
     createTexture()
-
-    // // const modelViewerTexture = document.querySelector('#logo');
-    // const orbitCycle = [
-    //     '14deg 0deg 0m',
-    //     '55deg 0deg 0m',
-    //     modelViewerTexture.cameraOrbit
-    // ];
-
-    // setInterval(() => {
-    //     const currentOrbitIndex = orbitCycle.indexOf(modelViewerTexture.cameraOrbit);
-    //     modelViewerTexture.cameraOrbit =
-    //         orbitCycle[(currentOrbitIndex + 1) % orbitCycle.length];
-    // }, 3000);
 })
