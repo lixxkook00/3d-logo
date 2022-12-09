6e25210f0a3e7c70b28b36b6f29b5db6db0413b7
Linh <lixxkook@Macbut-M1-cua-Linh.local>
2022-12-09 13:17:36 +0700

    dcmm chinh sua hoai

--- main.js
+++ main.js
@@ -1,36 +1,35 @@
-const modelViewerTexture = document.querySelector("#logo");
+// const modelViewerTexture = document.querySelector("#logo");
 const modelViewerTexture2 = document.querySelector("#logo-2");
 
-modelViewerTexture.addEventListener("load", () => {
-    const material = modelViewerTexture.model.materials[0];
-    const material2 = modelViewerTexture.model.materials[1];
+// modelViewerTexture.addEventListener("load", () => {
+//     const material = modelViewerTexture.model.materials[0];
+//     const material2 = modelViewerTexture.model.materials[1];
 
-    // material.pbrMetallicRoughness.setBaseColorFactor("../assest/do.png");
+//     // material.pbrMetallicRoughness.setBaseColorFactor("../assest/do.png");
 
-    const createTexture = async () => {
-        const textureRed = await modelViewerTexture.createTexture("./assets/do.png");    
-        const textureRed2 = await modelViewerTexture.createTexture("./assets/red.jpg");
+//     const createTexture = async () => {
+//         const textureRed = await modelViewerTexture.createTexture("./assets/do.png");    
+//         const textureRed2 = await modelViewerTexture.createTexture("./assets/red.jpg");
 
-        const textureBlue = await modelViewerTexture.createTexture("./assets/xanh.png");  
-        const textureBlue2 = await modelViewerTexture.createTexture("./assets/blue.jpg");  
+//         const textureBlue = await modelViewerTexture.createTexture("./assets/xanh.png");  
+//         const textureBlue2 = await modelViewerTexture.createTexture("./assets/blue.jpg");  
         
-        // console.log(material2.pbrMetallicRoughness["baseColorTexture"])
+//         // console.log(material2.pbrMetallicRoughness["baseColorTexture"])
         
-        // set texture for RED part :
-        material.pbrMetallicRoughness["baseColorTexture"].setTexture(textureRed);
-        material.pbrMetallicRoughness.metallicRoughnessTexture.setTexture(textureRed2);
-        material.occlusionTexture.setTexture(textureRed2);
+//         // set texture for RED part :
+//         material.pbrMetallicRoughness["baseColorTexture"].setTexture(textureRed);
+//         material.pbrMetallicRoughness.metallicRoughnessTexture.setTexture(textureRed2);
+//         material.occlusionTexture.setTexture(textureRed2);
 
-        // set texture for BLUE part :
-        material2.pbrMetallicRoughness["baseColorTexture"].setTexture(textureBlue2);
-        material2.pbrMetallicRoughness.setBaseColorFactor("#3da1ff");
-        material2.pbrMetallicRoughness.metallicRoughnessTexture.setTexture(textureBlue2);
-        // material2.occlusionTexture.setTexture(textureRed);
-    }
+//         // set texture for BLUE part :
+//         material2.pbrMetallicRoughness["baseColorTexture"].setTexture(textureBlue2);
+//         material2.pbrMetallicRoughness.setBaseColorFactor("#3da1ff");
+//         material2.pbrMetallicRoughness.metallicRoughnessTexture.setTexture(textureBlue2);
+//         // material2.occlusionTexture.setTexture(textureRed);
+//     }
 
-    createTexture()
-})
-
+//     createTexture()
+// })
 
 modelViewerTexture2.addEventListener("load", () => {
     const material = modelViewerTexture2.model.materials[0];
@@ -51,3 +50,22 @@ modelViewerTexture2.addEventListener("load", () => {
 
     createTexture()
 })
+
+// DCMM SLOGAN CONCAC
+const LOGO_WIDTH = modelViewerTexture2.offsetWidth
+const SLOGAN_INIT_WIDTH = 1069.6
+const SLOGAN_ELEMENT = document.querySelector("#slogan");
+
+const setWidthSlogan = () => {
+    let finalScaleRate = ((LOGO_WIDTH * 0.72)/1069.6)
+    console.log("finalScaleRate",finalScaleRate)
+    SLOGAN_ELEMENT.style.transform = `scale(${finalScaleRate}) translate(-46%,100px)`
+}
+
+window.onresize = () => {
+   setWidthSlogan()
+}
+
+window.onload = () => {
+    setWidthSlogan()
+}
