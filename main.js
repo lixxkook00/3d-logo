15eee24f159e4542f4af875c920199c4ce7190e9
Linh <lixxkook@Macbut-M1-cua-Linh.local>
2022-12-09 11:11:22 +0700

    update

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const modelViewerTexture = document.querySelector("#logo");
+const modelViewerTexture2 = document.querySelector("#logo-2");
 
 modelViewerTexture.addEventListener("load", () => {
     const material = modelViewerTexture.model.materials[0];
@@ -13,7 +14,7 @@ modelViewerTexture.addEventListener("load", () => {
         const textureBlue = await modelViewerTexture.createTexture("./assets/xanh.png");  
         const textureBlue2 = await modelViewerTexture.createTexture("./assets/blue.jpg");  
         
-        console.log(material2.pbrMetallicRoughness["baseColorTexture"])
+        // console.log(material2.pbrMetallicRoughness["baseColorTexture"])
         
         // set texture for RED part :
         material.pbrMetallicRoughness["baseColorTexture"].setTexture(textureRed);
@@ -25,6 +26,27 @@ modelViewerTexture.addEventListener("load", () => {
         material2.pbrMetallicRoughness.setBaseColorFactor("#3da1ff");
         material2.pbrMetallicRoughness.metallicRoughnessTexture.setTexture(textureBlue2);
         // material2.occlusionTexture.setTexture(textureRed);
+    }
+
+    createTexture()
+})
+
+
+modelViewerTexture2.addEventListener("load", () => {
+    const material = modelViewerTexture2.model.materials[0];
+    const material2 = modelViewerTexture2.model.materials[1];
+
+    // material.pbrMetallicRoughness.setBaseColorFactor("../assest/do.png");
+
+    const createTexture = async () => {
+        const textureRed = await modelViewerTexture2.createTexture("./assets/do.png");  
+        const textureBlue = await modelViewerTexture2.createTexture("./assets/xanh.png");    
+        
+        // set texture for RED part :
+        material.pbrMetallicRoughness["baseColorTexture"].setTexture(textureRed);
+
+        // set texture for BLUE part :
+        material2.pbrMetallicRoughness["baseColorTexture"].setTexture(textureBlue);
     }
 
     createTexture()
